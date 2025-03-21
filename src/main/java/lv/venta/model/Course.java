b8cb343da56244695c14823026121ab785a05fe3
NaurisJ <nauris.javalds@gmail.com>
2025-03-21 10:31:48 +0200

    Initial commit

--- src/main/java/lv/venta/model/Course.java
+++ src/main/java/lv/venta/model/Course.java
@@ -1,11 +1,14 @@
 package lv.venta.model;
 
+import java.util.Collection;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
@@ -46,6 +49,13 @@ public class Course {
 	@OneToOne
 	@JoinColumn(name = "PId")//jānorāda uz otras klases PK kolonnas nosaukumu
 	private Professor professor;
+	
+	@OneToMany(mappedBy = "course")
+	@ToString.Exclude
+	private Collection<Grade> grades;
+	
+	
+	
 	
 	public Course(String title, int cp, Professor professor) {
 		setTitle(title);
--- src/main/java/lv/venta/model/Grade.java
+++ src/main/java/lv/venta/model/Grade.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
@@ -40,6 +41,7 @@ public class Grade {
 	private Student student;
 	
 	@ManyToOne
+	@JoinColumn(name = "Cid")
 	private Course course;
 	
 	
